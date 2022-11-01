37c425fac4034f592047edd34892ed0c13a7aad1
Averand <leonk8867@gmail.com>
2022-11-01 11:26:59 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import "./styles.css";
 import { useState } from "react";
 
-function MyButton() {
+function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
+}
+
+export default function App() {
+  /* Each MyButton Component doesn't have it's own seperate 
+  / counter */
   const [count, setCount] = useState(1);
 
   function handleClick() {
     setCount(count + 1);
   }
-
-  return <button onClick={handleClick}>Clicked {count} times</button>;
-}
-
-export default function App() {
-  /* Each MyButton Component has it's own seperate 
-  / counter */
   return (
     <div>
       <h1>Hello</h1>
-      <MyButton /> c
+      <MyButton count={count} onClick={handleClick} />
       <br />
-      <MyButton />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
