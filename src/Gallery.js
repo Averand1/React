0ac0bfe212545fab070f1a3c0b8ca3f663762ed3
Averand <leonk8867@gmail.com>
2022-11-02 20:37:11 +0000

    Seperate Files for different files,

---
+++ src/Gallery.js
@@ -0,0 +1,10 @@
+import { Profile } from "./Profile.js";
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Vagabond</h1>
+      <Profile />
+      <Profile />
+    </div>
+  );
+}
---
+++ src/Profile.js
@@ -0,0 +1,8 @@
+export function Profile() {
+  return (
+    <img
+      src="https://cdn.myanimelist.net/images/characters/11/348750.jpg"
+      alt="Musashi Miyamoto"
+    />
+  );
+}
